4c68f0f70b6c13e233b70270f15c4c7ab367cafd
Maria Guerrero <guerrermj@gmail.com>
2020-04-26 17:17:01 -0400

    new directory

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-// 1. Show description and search form
-// 2. User will input their query and search button
-// 3. User clicks search button, desc. and form go away
-// 4. User clicks on new search button, results go away, and original home screen comes back
-
-// 2. fetch info from namus api // done
-// 3. then check response // done
-// 4. if invalid throw error //done
-// 5. if valid, display results
-// 6. catch error, display err message //done
-// 7. display new search button
-// 8. Show new search message to user
-// 9. On click Clear form
-// 10. Get form info on submit
-
 "use strict";
+
 let searchURL = `https://public.opendatasoft.com/api/records/1.0/search/?dataset=namus-missings&facet=statedisplaynameoflastcontact`;
 
-const startingElems = {
+const landingPage = {
   homeSection: `
   <section id="starting-screen">
     <h2 class="description">An app that provides you information on missing persons in your area or state. This app includes their last known location, 
@@ -24,129 +10,161 @@ const startingElems = {
     </h2>
     <form id="search-form">
       <label for="city" id="city">Enter City</label>
-      <input id="search-city" class="userInput" type="text" name="search-city" placeholder="Austin" autofocus spellcheck="true" required>
+      <input id="search-city" class="userInput" type="text" name="search-city" placeholder="Austin" focus spellcheck="true" required>
       <label for="state" id="state">Enter State</label>
-      <input id="search-state" class="userInput" type="text" name="search-state" placeholder="TX" maxlength="2" autofocus spellcheck="true" required>
+      <input id="search-state" class="userInput" type="text" name="search-state" placeholder="TX" maxlength="2" focus spellcheck="true" required>
       <button id="search">Search</button>
       </form>
   </section> `
 };
 
 $(document).ready(function() {
-  $(".js-form").append(startingElems.homeSection);
+  $(".js-form").append(landingPage.homeSection);
+  $(".results-container").hide();
+  $("#resources").hide();
 });
 
-//creates url with user input
+//combines searchURL with user input
 function formatQueryParams(params) {
   const queryItems = Object.keys(params).map(
     key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
   );
   return queryItems.join("&");
 }
-
 // 2. fetch info from namus api
+let cityQuery = "";
+
 function getMissingPerson(cityQuery, stateQuery) {
-  // let cityoflastcontact = cityQuery
-  let statedisplaynameoflastcontact = stateQuery;
+  cityQuery = cityQuery
+    .toLowerCase()
+    .split(" ")
+    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(" ");
+
   let params = {
     facet: "cityoflastcontact",
     "refine.cityoflastcontact": cityQuery,
-    "refine.statedisplaynameoflastcontact": stateQuery
+    "refine.statedisplaynameoflastcontact": stateQuery.toUpperCase(),
+    rows: 1000
   };
 
   const queryString = formatQueryParams(params);
   const url = `${searchURL}&${queryString}`;
 
-  console.log(url);
-
-  fetch(url)
+  Promise.all([fetch(url)])
     .then(res => {
-      if (res.ok) {
-        return res.json();
+      if (res[0].ok) {
+        return res[0].json();
       }
-      throw new Error(res.statusText);
+      throw new Error(res[0].statusText);
     })
-    .then(resJson => displayResults(resJson))
+    .then(resJson => {
+      if (!resJson.records.length) {
+        $("#err").show();
+      } else {
+        displayResults(resJson);
+      }
+    })
     .catch(err => {
       $("#js-error-message").text(`Something went wrong: ${err.message}`);
     });
+}
+//get random array of MP's
+function shuffleArray(resJson) {
+  for (let i = resJson.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [resJson[i], resJson[j]] = [resJson[j], resJson[i]];
+  }
+  return resJson;
 }
 
 // 5. displayResults by emptying landing page and filling it with results
 function displayResults(resJson) {
   // if there are previous results, remove them
-  console.log(resJson);
   $("#results-list").empty();
   $("#starting-screen").hide();
   // iterate through the items array
-  for (let i = 0; i < resJson.records.length; i++) {
-    // for each record object in the items
-    //array, add a list item to the results
-    //list with the name, description,
-    //and image of MP
 
-    //get random array of MP's
-    if (resJson.length > 0) {
-      let random = getRandomMP(0, resJson.length - 1);
-      console.log(resJson[random]);
-    }
-    function getRandomMP(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    const fullName = `${resJson.records[i].fields.firstname} ${resJson.records[i].fields.middlename}  ${resJson.records[i].fields.lastname}`;
-    const lastLocation = `${resJson.records[i].fields.cityoflastcontact}, ${resJson.records[i].fields.statedisplaynameoflastcontact}`;
+  let records = shuffleArray(resJson.records);
+
+  for (let i = 0; i < records.length; i++) {
+    const {
+      firstname,
+      middlename,
+      lastname,
+      cityoflastcontact,
+      statedisplaynameoflastcontact,
+      dateoflastcontact,
+      link,
+      image,
+      raceethnicity
+    } = records[i].fields;
+
+    const fullName = `${firstname} ${
+      middlename ? middlename : ""
+    }  ${lastname}`;
+    const lastLocation = `${cityoflastcontact}, ${statedisplaynameoflastcontact}`;
+    const img =
+      "https://public.opendatasoft.com/explore/dataset/namus-missings/files/" +
+      image.id +
+      "/300/";
+    const contact = link + "/contacts";
 
     $("#results-list").append(`
-    <section class="mp-info">
-    <div>
+    <section class="mp-container">
+    <ul class="mp-info">
       <li>
-        <h3>${fullName}</h3>
+      <h3 class="fullName">${fullName}</h3>
       </li>
-      </div>
-      <div>
-      <li>    
-        <p>
-         <img src=${resJson.records[i].fields.image}>
-        </p>
-      </li> 
-      </div> 
-      <div>
-      <li>    
-        <h4>
-          Last seen: ${lastLocation} on ${resJson.records[i].fields.dateoflastcontact}
-        </h4>
+      
+      <li><br /> <img src=${img} />
       </li>
-      </div>
-      <div>
+      <li>  <h4 class="race"> Race/Ethnicity: ${raceethnicity} </h4>
+      </li>
+      <li>
+      <h4 class="lastSeen"> Last seen: ${lastLocation} on ${dateoflastcontact}</h4>
+      </li>
+    
       <li> 
         <h4 class="link"> 
-        Find out more information: <a href=${resJson.records[i].fields.link}>${resJson.records[i].fields.link}</a>
+        Find out more information: <a href=${link}>${link}</a>
         </h4> 
       </li>
-      </div>
+    </ul>
+  
     </section>
       `);
+
+    $("#resources").show();
+    //     $("#contact-list").append(`<h4>Investigating Agencies: ${contact}
+    // </h4>`);
   }
+
+  $;
   // 7. display new search button
   // 8. Show CTA message to user
   $("#new-search-msg").html(
-    `<p class="message footer">Want to search in a new location?</p>
+    `<p class="message-footer">Want to search in a new location?</p>
       <button id="new-search-btn">Try again</button>`
   );
   //display the results section
+  $(".results-txt").removeClass("hidden");
+  $(".results-container")
+    .removeClass("hidden")
+    .show();
   $(".results").show();
   $("#new-search-msg").show();
 }
-
 // 9. On click Clear form
 // 10. Get form info on submit
 function getnewSearch() {
   $("body").on("click", "#new-search-btn", function(e) {
     e.preventDefault();
-    $(".results").hide();
+    $(".results-container").hide();
+    $(".results-txt").hide();
     $("#new-search-msg").hide();
     $("#starting-screen").show();
-    $(".userInput").empty();
+    $(".userInput").val("");
   });
 }
 
